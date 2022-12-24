5211338136c27c964d545cd84a402d259f77e3a2
pramo <pramodkumarp6@gmail.com>
2022-12-24 12:17:56 +0530

    OtpSend

---
+++ lib/views/forgetuser/otp_send_verify.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class OtpSendVerify extends StatefulWidget {
+  const OtpSendVerify({Key? key}) : super(key: key);
+
+  @override
+  State<OtpSendVerify> createState() => _OtpSendVerifyState();
+}
+
+class _OtpSendVerifyState extends State<OtpSendVerify> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
