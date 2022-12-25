676882c8a51a77b6e8c4a82d51fc731d87f56487
pramo <pramodkumarp6@gmail.com>
2022-12-25 23:34:37 +0530

    initial

--- lib/src/res/components/roundButton.dart
+++ lib/src/res/components/roundButton.dart
@@ -17,23 +17,26 @@ class RoundButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      decoration: BoxDecoration(
-          color: Colors.purple, borderRadius: BorderRadius.circular(10)),
-      child: loading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Center(
-              child: Text(
-              title,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline2!
-                  .copyWith(fontSize: 16, color: textColor),
-            )),
+    return InkWell(
+      onTap: loading ? null : onPress,
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.purple, borderRadius: BorderRadius.circular(10)),
+        child: loading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Center(
+                child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline2!
+                    .copyWith(fontSize: 16, color: textColor),
+              )),
+      ),
     );
   }
 }
