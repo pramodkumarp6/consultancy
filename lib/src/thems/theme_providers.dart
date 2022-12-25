5c9b70b6589157e774f0190b48c633f9894970c3
pramo <pramodkumarp6@gmail.com>
2022-12-25 17:51:19 +0530

    Setting

---
+++ lib/src/thems/theme_providers.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyThemes {
+  static final darkTheme =
+      ThemeData(scaffoldBackgroundColor: Colors.grey.shade100);
+
+  static final lightTheme = ThemeData(scaffoldBackgroundColor: Colors.white);
+}
