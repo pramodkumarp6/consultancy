ff895ee5a8a1e938f18444ee3a3fc2ae4ac2623a
pramo <pramodkumarp6@gmail.com>
2022-12-26 01:29:08 +0530

    initial

--- lib/src/res/components/inputButton.dart
+++ lib/src/res/components/inputButton.dart
@@ -1,10 +1,24 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class InputTextField extends StatelessWidget {
-  const InputTextField({Key? key}) : super(key: key);
+  final FocusNode focusNode;
+  final String hint;
+  final bool obscureText;
+  final TextInputType keyBoardType;
+  final bool enable, autoFoucs;
+  final FormFieldValidator fieldValidator;
+
+  const InputTextField({
+    Key? key,
+    required this.focusNode,
+    required this.keyBoardType,
+    required this.hint,
+    required this.obscureText,
+    required this.fieldValidator,
+    this.autoFoucs = false,
+    this.enable = true,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
