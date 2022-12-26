7cde33fd899078d0f8009c3bb12462a7a12de47d
pramo <pramodkumarp6@gmail.com>
2022-12-26 16:58:32 +0530

    initial

--- lib/src/res/components/inputButton.dart
+++ lib/src/res/components/inputButton.dart
@@ -2,26 +2,45 @@
 import 'package:flutter/material.dart';
 
 class InputTextField extends StatelessWidget {
+  final TextEditingController myController;
+  final FormFieldSetter onFieldSubmittedValue;
   final FocusNode focusNode;
   final String hint;
   final bool obscureText;
   final TextInputType keyBoardType;
   final bool enable, autoFoucs;
-  final FormFieldValidator fieldValidator;
+  final FormFieldValidator onValidator;
 
   const InputTextField({
     Key? key,
     required this.focusNode,
+    required this.myController,
+    required this.onFieldSubmittedValue,
     required this.keyBoardType,
     required this.hint,
     required this.obscureText,
-    required this.fieldValidator,
+    required this.onValidator,
     this.autoFoucs = false,
     this.enable = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: TextFormField(
+        controller: myController,
+        focusNode: focusNode,
+        onFieldSubmitted: onFieldSubmittedValue,
+        obscureText: obscureText,
+        validator: onValidator,
+        keyboardType: keyBoardType,
+        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(10),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.all(Radius.circular(8)))),
+      ),
+    );
   }
 }
--- lib/src/views/login/login_screen.dart
+++ lib/src/views/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:consultancy/src/res/components/inputButton.dart';
 import 'package:consultancy/src/res/components/roundButton.dart';
 import 'package:consultancy/src/utils/toasty.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final emailController = TextEditingController();
+  final emailFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     // final authViewModel = Provider.of<AuthViewModel>(context);
@@ -20,20 +23,35 @@ class _LoginState extends State<Login> {
         title: const Text('Login'),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          RoundButton(
-            title: 'Login',
-            loading: false,
-            onPress: () {
-              Navigator.pushNamed(context, RoutesName.home);
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InputTextField(
+                focusNode: emailFocusNode,
+                myController: emailController,
+                onFieldSubmittedValue: (value) {},
+                enable: true,
+                keyBoardType: TextInputType.emailAddress,
+                hint: 'Email',
+                obscureText: false,
+                onValidator: (value) {
+                  print(value);
+                  return value.isEmpty ? 'enter Email' : null;
+                }),
+            RoundButton(
+              title: 'Login',
+              loading: false,
+              onPress: () {
+                Navigator.pushNamed(context, RoutesName.home);
 
-              Toasty.toastMessage('Login Success');
-            },
-          )
-        ],
+                Toasty.toastMessage('Login Success');
+              },
+            )
+          ],
+        ),
       ),
       // body: Center(
       //   child: InkWell(
